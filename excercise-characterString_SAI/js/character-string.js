25dc9018cd38777fbf344846a79adf723d376d97
Sai Varadutt Kumar Suthari <ssuthari@isimtl.local>
2020-02-28 13:38:27 -0500

    Exercise Character String

---
+++ excercise-characterString_SAI/js/character-string.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const SENTENCE = 'The strength of JavaScript is that you can do anything. The weakness is that you will.'
+
+console.log('The whole sentence:', SENTENCE)
+
+console.log('The length of the string:', SENTENCE.length)
+
+console.log('The position of the word "JavaScript":',SENTENCE.indexOf("JavaScript"))
+
+console.log('The position of the last \'is\':', SENTENCE.lastIndexOf("is"))
+
+console.log('The last sentence only:', SENTENCE.split(". ")[1])
+
+console.log('The 10 characters starting from the 17th character:', SENTENCE.substr(17, 10))
+
+console.log('The number of words in the whole phrase:',SENTENCE.split(" ").length)
+
+console.log('The 61st character:', SENTENCE.charAt('61'))
+
+console.log('Replace \'you\' with \'I\' in the whole phrase:', SENTENCE.replace(/you/g,'I'))
